645c340919812d012259090f69477f2b1417238f
Alex Pierini <alex_pierini@hotmail.com>
2024-01-06 00:31:21 -0500

    SideMenu component

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,20 +1,27 @@
 import Link from 'next/link'
+import { MouseEvent } from 'react'
 
 type MenuItemProps = {
-  url: string
+  url?: string
   text: string
   icon: JSX.Element
+  onClick?: (event: MouseEvent<HTMLElement>) => void
+  className?: string
 }
 
 export default function MenuItem(props: MenuItemProps) {
+  const linkContentRender = () => (
+    <div
+      className={`flex h-20 w-20 flex-col items-center justify-center  text-gray-600 ${props.className}`}
+    >
+      {props.icon}
+      <span className="text-xs font-light">{props.text}</span>
+    </div>
+  )
+
   return (
-    <li className="hover:bg-gray-100">
-      <Link href={props.url}>
-        <div className="flex h-20 w-20 flex-col items-center justify-center">
-          {props.icon}
-          <span className="text-xs font-light text-gray-600">{props.text}</span>
-        </div>
-      </Link>
+    <li onClick={props.onClick} className="hover:bg-gray-100 cursor-pointer">
+      {props.url ? <Link href={props.url}>{linkContentRender()}</Link> : linkContentRender()}
     </li>
   )
 }
--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,17 +1,25 @@
-import { AdjustmentsVerticalIcon, BellIcon, HomeIcon } from '../icons'
+import { AdjustmentsVerticalIcon, ArrowLeftEndOnRectangle, BellIcon, HomeIcon } from '../icons'
 import Logo from './Logo'
 import MenuItem from './MenuItem'
 
 export default function SideMenu() {
   return (
-    <aside>
+    <aside className="flex flex-col">
       <div className="flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20">
         <Logo />
       </div>
-      <ul>
+      <ul className="flex-grow">
         <MenuItem url="/" text="Home" icon={HomeIcon} />
         <MenuItem url="/settings" text="Settings" icon={AdjustmentsVerticalIcon} />
         <MenuItem url="/notifications" text="Notifications" icon={BellIcon} />
+      </ul>
+      <ul>
+        <MenuItem
+          text="Logout"
+          icon={ArrowLeftEndOnRectangle}
+          onClick={() => alert('logout')}
+          className="text-red-600 hover:bg-red-400 hover:text-white"
+        />
       </ul>
     </aside>
   )
