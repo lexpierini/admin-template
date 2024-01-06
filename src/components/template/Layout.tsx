59e704f1520f4975208efd7e345447a4a8a60aa2
Alex Pierini <alex_pierini@hotmail.com>
2024-01-06 17:23:44 -0500

    Context API

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react'
 import Content from './Content'
 import Header from './Header'
 import SideMenu from './SideMenu'
+import useAppData from '@/data/context/hook/useAppData'
 
 type LayoutProps = {
   title: string
@@ -11,8 +12,10 @@ type LayoutProps = {
 }
 
 export default function Layout(props: LayoutProps) {
+  const { theme, changeTheme } = useAppData()
+
   return (
-    <div className="dark flex h-screen w-screen">
+    <div className={`${theme} flex h-screen w-screen`}>
       <SideMenu />
       <div className="flex w-full flex-col bg-gray-300 p-7 dark:bg-gray-800">
         <Header title={props.title} subtitle={props.subtitle} />
---
+++ src/data/context/AppContext.tsx
@@ -0,0 +1,34 @@
+import { ReactNode, createContext, useState } from 'react'
+
+type AppProviderProps = {
+  children: ReactNode
+}
+
+type Theme = string | ''
+
+type AppContextProps = {
+  theme?: Theme
+  changeTheme?: () => void
+}
+
+const AppContext = createContext<AppContextProps>({})
+
+export function AppProvider(props: AppProviderProps) {
+  const [theme, setTheme] = useState<Theme>('')
+
+  const changeTheme = () => {
+    setTheme(theme === '' ? 'dark' : '')
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        theme,
+        changeTheme,
+      }}
+    >
+      {props.children}
+    </AppContext.Provider>
+  )
+}
+export default AppContext
