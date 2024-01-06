12756bae3373d1d5b8de3b17ceb272f807ac79fe
Alex Pierini <alex_pierini@hotmail.com>
2024-01-05 23:32:00 -0500

    Logo component

--- src/components/template/SideMenu.tsx
+++ src/components/template/SideMenu.tsx
@@ -1,9 +1,13 @@
 import { AdjustmentsVerticalIcon, BellIcon, HomeIcon } from '../icons'
+import Logo from './Logo'
 import MenuItem from './MenuItem'
 
 export default function SideMenu() {
   return (
     <aside>
+      <div className="flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20">
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" text="Home" icon={HomeIcon} />
         <MenuItem url="/settings" text="Settings" icon={AdjustmentsVerticalIcon} />
