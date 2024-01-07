c94b736958fcde0e2ba266d0eaa1d979a3710f64
Alex Pierini <alex_pierini@hotmail.com>
2024-01-07 18:03:52 -0500

    Cookie section

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,7 +1,8 @@
 import User from '@/model/User'
 import firebase from '../../firebase/config'
-import { ReactNode, createContext, useState } from 'react'
+import { ReactNode, createContext, useEffect, useState } from 'react'
 import route from 'next/router'
+import Cookies from 'js-cookie'
 
 type AuthProviderProps = {
   children: ReactNode
@@ -26,18 +27,45 @@ const normalizedUser = async (firebaseUser: firebase.User): Promise<User> => {
   }
 }
 
+const cookieManagement = (logged: boolean) => {
+  if (logged) {
+    Cookies.set('admin-template-lexpierini-auth', logged, {
+      expires: 7,
+    })
+  } else {
+    Cookies.remove('admin-template-lexpierini-auth')
+  }
+}
+
 export function AuthProvider(props: AuthProviderProps) {
+  const [loading, setLoading] = useState<boolean>(true)
   const [user, setUser] = useState<User>(null)
+
+  const sectionSetup = async (firebaseUser: firebase.User) => {
+    if (firebaseUser?.email) {
+      const user = await normalizedUser(firebaseUser)
+      setUser(user)
+      cookieManagement(true)
+      setLoading(false)
+      return user.email
+    } else {
+      setUser(null)
+      cookieManagement(false)
+      setLoading(false)
+      return false
+    }
+  }
 
   const googleLogin = async () => {
     const res = await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
-
-    if (res.user?.email) {
-      const user = await normalizedUser(res.user)
-      setUser(user)
-      route.push('/')
-    }
+    sectionSetup(res.user)
+    route.push('/')
   }
+
+  useEffect(() => {
+    const cancel = firebase.auth().onIdTokenChanged(sectionSetup)
+    return () => cancel()
+  }, [])
 
   return <AuthContext.Provider value={{ user, googleLogin }}>{props.children}</AuthContext.Provider>
 }
