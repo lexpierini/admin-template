68229f14d25e6ef1dce7e88d1de5a12db3c34f8d
Alex Pierini <alex_pierini@hotmail.com>
2024-01-07 20:28:13 -0500

    Fix eslint errors

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -1,10 +1,10 @@
 export default function Logo() {
   return (
-    <div className="flex flex-col items-center justify-center h-12 w-12 rounded-full bg-white">
-      <div className="h-3 w-3 rounded-full bg-red-600 mb-0.5" />
-      <div className="flex mt-0.5">
-        <div className="h-3 w-3 rounded-full bg-yellow-500 mr-0.5" />
-        <div className="h-3 w-3 rounded-full bg-green-600 ml-0.5" />
+    <div className="flex h-12 w-12 flex-col items-center justify-center rounded-full bg-white">
+      <div className="mb-0.5 h-3 w-3 rounded-full bg-red-600" />
+      <div className="mt-0.5 flex">
+        <div className="mr-0.5 h-3 w-3 rounded-full bg-yellow-500" />
+        <div className="ml-0.5 h-3 w-3 rounded-full bg-green-600" />
       </div>
     </div>
   )
